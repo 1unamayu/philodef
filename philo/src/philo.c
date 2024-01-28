e3a6ba780fec5fd4a6287c22ebddd7ab8a285760
xamayuel Mint <unamayu@gmail.com>
2024-01-28 20:26:41 +0100

    joder lo que esta costando otra vez

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 10:52:04 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/01/28 19:03:01 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/01/28 20:25:29 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,9 @@ t_person *ft_persons_create(t_game_data *data)
 	cont = 0;
 	
 	data->screen = malloc(sizeof(pthread_mutex_t));
-	persons = malloc(sizeof(t_person)*data->num_persons);
 	pthread_mutex_init(data->screen,NULL);
+	persons = malloc(sizeof(t_person)*data->num_persons);
+	data->thread_person=malloc(sizeof(pthread_t)*data->num_persons); //quitar este 200
 	while (cont < data->num_persons)
 	{
 		//printf("inicializando persona %d \n",cont+1);
@@ -58,7 +59,9 @@ t_person *ft_persons_create(t_game_data *data)
 		(persons+cont)->rfork = ((cont +1) % data->num_persons);
 		(persons+cont)->is_death= 0;
 		(persons+cont)->data = data;
-		printf("Position            :%d\n", (persons+cont)->position);
+		(persons+cont)->eating = malloc(sizeof(pthread_mutex_t));
+		pthread_mutex_init((persons+cont)->eating,NULL);
+		//printf("Position            :%d\n", (persons+cont)->position);
 		//printf("Left fork position  :%d\n", persons->lfork);
 		//printf("Right fork position :%d\n", persons->rfork);
 		//printf("Time to eat         :%d\n", persons->data->time_to_die);
@@ -82,36 +85,55 @@ void ft_delete_game(t_game_data *data)
 void ft_take_forks(t_person *person)
 {
 	//printf("izq: %d\n", person->lfork);
-	//pthread_mutex_lock(&person->data->forks[person->lfork]);
+	pthread_mutex_lock(&person->data->forks[person->lfork]);
 	ft_report_screen(0,person);
 	//printf("dcha: %d\n", person->rfork);
-	//pthread_mutex_lock(&person->data->forks[person->rfork]);
+	pthread_mutex_lock(&person->data->forks[person->rfork]);
 	ft_report_screen(0,person);
 }
 
 void ft_eat(t_person *person)
 {
+	pthread_mutex_lock(person->eating);
+	ft_report_screen(1, person);
+	person->num_meals++;
+	person->next_meal_timestamp = ft_current_time()+person->data->time_to_die;
+	usleep(person->data->time_eating * 1000);
 	
+	pthread_mutex_unlock(&person->data->forks[person->lfork]);
+	pthread_mutex_unlock(&person->data->forks[person->rfork]);
+	pthread_mutex_unlock(person->eating);
+}
+
+void ft_sleep(t_person *person)
+{
+	usleep(10);
+	ft_report_screen(2,person);
+	usleep(person->data->time_sleeping * 1000);
 }
 void *ft_vida(void *arg)
 {
 	t_person *person;
 
 	person = arg;
-	//printf("Time to eat %d \n", person->data->time_to_die);
+	
 	person->next_meal_timestamp = ft_current_time()+person->data->time_to_die;
-	//printf("Time next eat %lld \n", person->next_meal_timestamp);
-	//printf("En vida %d\n",person->position);
+	
 
-	while (42)
+	while (1)
 	{
+		
 		ft_take_forks(person);
 		
-		printf("Eat\n");
-		printf("eating\n");
-		printf("sleeping\n");
-		printf("thinking\n");
+		ft_eat(person);
+
+		ft_sleep(person);
+		
+		ft_report_screen(3, person);
+		
+		
 	}
+	
 	return NULL;
 }
 
@@ -120,7 +142,7 @@ void ft_init_game_data(t_game_data *data)
 	int cont;
 
 	cont = 0;
-	data->thread_person = malloc(sizeof(pthread_t)*data->num_persons);
+	
 	data->forks = malloc(sizeof(pthread_mutex_t)*data->num_persons);
 	while (cont < data->num_persons)
 	{
@@ -156,21 +178,23 @@ int	main(int argn, char *argv[])
 	ft_init_game_data(&game_data);
 	
 	cont = 0;
+	game_data.thread_person = malloc(sizeof(pthread_t)*game_data.num_persons);
 	while (cont < game_data.num_persons)
 	{
 		//printf("vida %d \n", cont);
+		//pthread_create(game_data.thread_person+cont, NULL, ft_vida, persons+cont);
 		pthread_create(game_data.thread_person+cont, NULL, ft_vida, persons+cont);
 		pthread_detach(game_data.thread_person[cont]);
+		//pthread_detach(game_data.thread_person+cont);
 		cont ++;
 		usleep(100);
 		
 	}
-	cont = 0;
-	while (cont < game_data.num_persons)
-	{
-		printf("fuera %d\n", persons[cont].position);
-		cont++;
-	}
+	while(1) {
+        // Hacer otras tareas en el programa principal
+        // En este caso, simplemente mantenemos el programa principal en un bucle infinito
+    }
+	
 	ft_delete_all(persons);
 	ft_delete_game(&game_data);
 	//system("valgrind --leak-check=full ./philo");
